c5514917b770ebce19d2bcb85c5a9ea31fd89b32
Logan <kmartshopper@gmail.com>
2014-03-22 11:02:20 -0700

    Version update.

--- carpentersblocks/CarpentersBlocks.java
+++ carpentersblocks/CarpentersBlocks.java
@@ -21,7 +21,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 public class CarpentersBlocks {
     
     public static final String MODID   = "CarpentersBlocks";
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "3.0.1";
     public static FMLEventChannel channel;
     public static CreativeTabs creativeTab = new CarpentersBlocksTab(MODID);
     
