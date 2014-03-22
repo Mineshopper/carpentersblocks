6840eb4fbf844f4b19edd8390dfad04bd5758e5e
Logan <kmartshopper@gmail.com>
2014-03-21 22:48:33 -0700

    Adapt logger to new system.

--- carpentersblocks/util/ModLogger.java
+++ carpentersblocks/util/ModLogger.java
@@ -1,15 +1,49 @@
 package carpentersblocks.util;
 
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import carpentersblocks.CarpentersBlocks;
 import cpw.mods.fml.common.FMLLog;
 
 public class ModLogger {
     
-    public static void log(Level level, String message)
+    private static Logger logger = LogManager.getLogger(CarpentersBlocks.MODID);
+
+    public static void log(Level level, String format, Object... data)
     {
-        FMLLog.log(level, "[" + CarpentersBlocks.MODID + "] " + message);
+        logger.log(level, format, data);
+    }
+
+    public static void severe(String format, Object... data)
+    {
+        log(Level.ERROR, format, data);
+    }
+
+    public static void warning(String format, Object... data)
+    {
+        log(Level.WARN, format, data);
+    }
+
+    public static void info(String format, Object... data)
+    {
+        log(Level.INFO, format, data);
+    }
+
+    public static void fine(String format, Object... data)
+    {
+        log(Level.DEBUG, format, data);
+    }
+
+    public static void finer(String format, Object... data)
+    {
+        log(Level.TRACE, format, data);
+    }
+
+    public static Logger getLogger()
+    {
+        return logger;
     }
     
 }
