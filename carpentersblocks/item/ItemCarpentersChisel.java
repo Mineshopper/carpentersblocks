d35da1968b8c3ac759fccd9772b5a4eb07a02864
Logan <kmartshopper@gmail.com>
2014-04-20 13:05:02 -0700

    Move some common stuff out of tool classes.  Fixes tools destroying carpenters blocks.

--- carpentersblocks/item/ItemCarpentersChisel.java
+++ carpentersblocks/item/ItemCarpentersChisel.java
@@ -1,30 +1,15 @@
 package carpentersblocks.item;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import carpentersblocks.CarpentersBlocks;
 import carpentersblocks.api.ICarpentersChisel;
-import carpentersblocks.block.BlockCoverable;
-import carpentersblocks.util.registry.ItemRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemCarpentersChisel extends Item implements ICarpentersChisel {
-    
-    public ItemCarpentersChisel()
-    {
-        setMaxStackSize(1);
-        setCreativeTab(CarpentersBlocks.creativeTab);
-        
-        if (ItemRegistry.itemCarpentersToolsDamageable) {
-            setMaxDamage(ItemRegistry.itemCarpentersToolsUses);
-        }
-    }
-    
+public class ItemCarpentersChisel extends ItemBase implements ICarpentersChisel {
+
     @SideOnly(Side.CLIENT)
     @Override
     public void registerIcons(IIconRegister iconRegister)
@@ -42,16 +27,6 @@ public class ItemCarpentersChisel extends Item implements ICarpentersChisel {
     public boolean canUseChisel(World world, EntityPlayer entityPlayer)
     {
         return true;
-    }
-
-    /**
-     * Returns the strength of the stack against a given block. 1.0F base, (Quality+1)*2 if correct blocktype, 1.5F if
-     * sword
-     */
-    @Override
-    public float func_150893_a(ItemStack itemStack, Block block)
-    {
-        return block instanceof BlockCoverable ? 1.0001F : super.func_150893_a(itemStack, block);
     }
     
 }
--- carpentersblocks/item/ItemCarpentersHammer.java
+++ carpentersblocks/item/ItemCarpentersHammer.java
@@ -1,30 +1,15 @@
 package carpentersblocks.item;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import carpentersblocks.CarpentersBlocks;
 import carpentersblocks.api.ICarpentersHammer;
-import carpentersblocks.block.BlockCoverable;
-import carpentersblocks.util.registry.ItemRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemCarpentersHammer extends Item implements ICarpentersHammer {
-    
-    public ItemCarpentersHammer()
-    {
-        setMaxStackSize(1);
-        setCreativeTab(CarpentersBlocks.creativeTab);
-        
-        if (ItemRegistry.itemCarpentersToolsDamageable) {
-            setMaxDamage(ItemRegistry.itemCarpentersToolsUses);
-        }
-    }
-    
+public class ItemCarpentersHammer extends ItemBase implements ICarpentersHammer {
+
     @SideOnly(Side.CLIENT)
     @Override
     public void registerIcons(IIconRegister iconRegister)
@@ -42,26 +27,6 @@ public class ItemCarpentersHammer extends Item implements ICarpentersHammer {
     public boolean canUseHammer(World world, EntityPlayer entityPlayer)
     {
         return true;
-    }
-    
-    /**
-     * Returns the strength of the stack against a given block. 1.0F base, (Quality+1)*2 if correct blocktype, 1.5F if
-     * sword
-     */
-    @Override
-    public float func_150893_a(ItemStack itemStack, Block block)
-    {
-        return block instanceof BlockCoverable ? 1.0001F : super.func_150893_a(itemStack, block);
-    }
-    
-    /**
-     * Callback for item usage. If the item does something special on right clicking, he will have one of those. Return
-     * True if something happen and false if it don't. This is for ITEMS, not BLOCKS
-     */
-    @Override
-    public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
-    {
-        return false;
     }
 
 }
