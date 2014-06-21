36989daf3ecb73eb239b8e9c40724599e55004ea
Logan <kmartshopper@gmail.com>
2014-06-21 10:12:45 -0700

    Version update.

--- carpentersblocks/CarpentersBlocks.java
+++ carpentersblocks/CarpentersBlocks.java
@@ -32,7 +32,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 public class CarpentersBlocks {
 
 	public final static String MODID = "CarpentersBlocks";
-	public final static String VERSION = "2.1.0";
+	public final static String VERSION = "2.1.1";
 
     @Instance(MODID)
     public static CarpentersBlocks instance;
