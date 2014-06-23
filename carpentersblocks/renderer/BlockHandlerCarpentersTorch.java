e39a5cd9cab94cdf6270e9020f7c38323be110d3
Logan <kmartshopper@gmail.com>
2014-06-23 14:32:34 -0700

    Fix lighting issue with torch.

--- carpentersblocks/renderer/BlockHandlerCarpentersTorch.java
+++ carpentersblocks/renderer/BlockHandlerCarpentersTorch.java
@@ -16,7 +16,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
 
-    private Vec3[] vector = new Vec3[8];
+    private Vec3[] vec3 = new Vec3[8];
 
     @Override
     public boolean shouldRender3DInInventory(int modelId)
@@ -48,6 +48,7 @@ public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
         renderBlocks.renderAllFaces = true;
         disableAO = true;
 
+        renderBlocks.setRenderBounds(0.4375D, 0.0D, 0.4375D, 0.5625D, 0.625D, 0.5625D);
         renderTorch(getCoverForRendering(), x, y, z);
 
         disableAO = false;
@@ -60,7 +61,7 @@ public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
      */
     protected void render(int x, int y, int z, int side, IIcon icon)
     {
-        renderFace(Tessellator.instance, side, icon, true);
+        renderFace(side, icon, true);
     }
 
     /**
@@ -99,19 +100,21 @@ public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
         float vecY = 10.0F / 16.0F;
         float vecZ = 0.0625F;
 
-        vector[0] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.5D, -vecZ);
-        vector[1] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.5D, -vecZ);
-        vector[2] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.5D, vecZ);
-        vector[3] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.5D, vecZ);
-        vector[4] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, -vecZ);
-        vector[5] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, -vecZ);
-        vector[6] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, vecZ);
-        vector[7] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, vecZ);
+        vec3 = new Vec3[] {
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.5D, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.5D, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.5D, vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.5D, vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, vecZ)
+        };
 
-        setRotations(Torch.getFacing(TE), x, y, z);
+        setRotations(Torch.getFacing(TE), vec3, x, y, z);
 
         for (int side = 0; side < 6; ++side) {
-            renderFace(tessellator, side, icon, false);
+            renderFace(side, icon, false);
         }
     }
 
@@ -124,16 +127,18 @@ public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
         float vecY = 0.5F;
         float vecZ = 0.0625F;
 
-        vector[0] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.0D, -vecZ);
-        vector[1] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.0D, -vecZ);
-        vector[2] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.0D, vecZ);
-        vector[3] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.0D, vecZ);
-        vector[4] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, -vecZ);
-        vector[5] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, -vecZ);
-        vector[6] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, vecZ);
-        vector[7] = renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, vecZ);
+        vec3 = new Vec3[] {
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.0D, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.0D, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, 0.0D, vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, 0.0D, vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, -vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(vecX, vecY, vecZ),
+            renderBlocks.blockAccess.getWorldVec3Pool().getVecFromPool(-vecX, vecY, vecZ)
+        };
 
-        setRotations(Torch.getFacing(TE), x, y, z);
+        setRotations(Torch.getFacing(TE), vec3, x, y, z);
 
         lightingHelper.setupLightingYNeg(itemStack, x, y, z);
         delegateSideRender(itemStack, x, y, z, DOWN);
@@ -154,43 +159,43 @@ public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
         delegateSideRender(itemStack, x, y, z, EAST);
     }
 
-    private void setRotations(ForgeDirection facing, int x, int y, int z)
+    private void setRotations(ForgeDirection facing, Vec3[] vec3, int x, int y, int z)
     {
         for (int vecCount = 0; vecCount < 8; ++vecCount)
         {
             if (facing.equals(ForgeDirection.UP)) {
 
-                vector[vecCount].xCoord += x + 0.5D;
-                vector[vecCount].yCoord += y;
-                vector[vecCount].zCoord += z + 0.5D;
+                vec3[vecCount].xCoord += x + 0.5D;
+                vec3[vecCount].yCoord += y;
+                vec3[vecCount].zCoord += z + 0.5D;
 
             } else {
 
-                vector[vecCount].zCoord += 0.0625D;
-                vector[vecCount].rotateAroundX(-((float)Math.PI * 3.4F / 9F));
+                vec3[vecCount].zCoord += 0.0625D;
+                vec3[vecCount].rotateAroundX(-((float)Math.PI * 3.4F / 9F));
 
-                vector[vecCount].yCoord -= 0.4375D;
-                vector[vecCount].rotateAroundX((float)Math.PI / 2F);
+                vec3[vecCount].yCoord -= 0.4375D;
+                vec3[vecCount].rotateAroundX((float)Math.PI / 2F);
 
                 switch (facing) {
                     case NORTH:
-                        vector[vecCount].rotateAroundY(0.0F);
+                        vec3[vecCount].rotateAroundY(0.0F);
                         break;
                     case SOUTH:
-                        vector[vecCount].rotateAroundY((float)Math.PI);
+                        vec3[vecCount].rotateAroundY((float)Math.PI);
                         break;
                     case WEST:
-                        vector[vecCount].rotateAroundY((float)Math.PI / 2F);
+                        vec3[vecCount].rotateAroundY((float)Math.PI / 2F);
                         break;
                     case EAST:
-                        vector[vecCount].rotateAroundY(-((float)Math.PI / 2F));
+                        vec3[vecCount].rotateAroundY(-((float)Math.PI / 2F));
                         break;
                     default: {}
                 }
 
-                vector[vecCount].xCoord += x + 0.5D;
-                vector[vecCount].yCoord += y + 0.1875F;
-                vector[vecCount].zCoord += z + 0.5D;
+                vec3[vecCount].xCoord += x + 0.5D;
+                vec3[vecCount].yCoord += y + 0.1875F;
+                vec3[vecCount].zCoord += z + 0.5D;
             }
         }
     }
@@ -198,7 +203,7 @@ public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
     /**
      * Performs final rendering of face.
      */
-    private void renderFace(Tessellator tessellator, int side, IIcon icon, boolean isHandle)
+    private void renderFace(int side, IIcon icon, boolean isHandle)
     {
         double uMin, uMax, vMin, vMax;
 
@@ -221,40 +226,40 @@ public class BlockHandlerCarpentersTorch extends BlockHandlerBase {
 
         switch (side) {
             case DOWN:
-                vertex1 = vector[0];
-                vertex2 = vector[1];
-                vertex3 = vector[2];
-                vertex4 = vector[3];
+                vertex1 = vec3[0];
+                vertex2 = vec3[1];
+                vertex3 = vec3[2];
+                vertex4 = vec3[3];
                 break;
             case UP:
-                vertex1 = vector[7];
-                vertex2 = vector[6];
-                vertex3 = vector[5];
-                vertex4 = vector[4];
+                vertex1 = vec3[7];
+                vertex2 = vec3[6];
+                vertex3 = vec3[5];
+                vertex4 = vec3[4];
                 break;
             case NORTH:
-                vertex1 = vector[1];
-                vertex2 = vector[0];
-                vertex3 = vector[4];
-                vertex4 = vector[5];
+                vertex1 = vec3[1];
+                vertex2 = vec3[0];
+                vertex3 = vec3[4];
+                vertex4 = vec3[5];
                 break;
             case SOUTH:
-                vertex1 = vector[3];
-                vertex2 = vector[2];
-                vertex3 = vector[6];
-                vertex4 = vector[7];
+                vertex1 = vec3[3];
+                vertex2 = vec3[2];
+                vertex3 = vec3[6];
+                vertex4 = vec3[7];
                 break;
             case WEST:
-                vertex1 = vector[0];
-                vertex2 = vector[3];
-                vertex3 = vector[7];
-                vertex4 = vector[4];
+                vertex1 = vec3[0];
+                vertex2 = vec3[3];
+                vertex3 = vec3[7];
+                vertex4 = vec3[4];
                 break;
             case EAST:
-                vertex1 = vector[2];
-                vertex2 = vector[1];
-                vertex3 = vector[5];
-                vertex4 = vector[6];
+                vertex1 = vec3[2];
+                vertex2 = vec3[1];
+                vertex3 = vec3[5];
+                vertex4 = vec3[6];
                 break;
         }
 
