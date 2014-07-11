eeb0dad3a675edfda2c07be4bc5db5f918782623
Logan <kmartshopper@gmail.com>
2014-07-10 23:54:40 -0700

    Correct sloppy flower pot tile entity code.

--- src/main/java/carpentersblocks/tileentity/TECarpentersFlowerPot.java
+++ src/main/java/carpentersblocks/tileentity/TECarpentersFlowerPot.java
@@ -6,8 +6,9 @@ import net.minecraft.nbt.NBTTagList;
 
 public class TECarpentersFlowerPot extends TEBase {
 
-    public static final byte TAG_SOIL  = 0;
-    public static final byte TAG_PLANT = 1;
+    private final String TAG_PLANT_ITEMSTACKS = "pot_property";
+    public final String TAG_SOIL  = "soil";
+    public final String TAG_PLANT = "plant";
 
     public ItemStack soil;
     public ItemStack plant;
@@ -17,10 +18,19 @@ public class TECarpentersFlowerPot extends TEBase {
     {
         super.readFromNBT(nbt);
 
-        NBTTagList list = nbt.getTagList("pot_property", 10);
+        NBTTagList list = nbt.getTagList(TAG_PLANT_ITEMSTACKS, 10);
 
-        soil = ItemStack.loadItemStackFromNBT(list.getCompoundTagAt(TAG_SOIL));
-        plant = ItemStack.loadItemStackFromNBT(list.getCompoundTagAt(TAG_PLANT));
+        soil = null;
+        plant = null;
+
+        for (int idx = 0; idx < list.tagCount(); ++idx)
+        {
+            if ((list.getCompoundTagAt(idx)).hasKey(TAG_SOIL)) {
+                soil = ItemStack.loadItemStackFromNBT(list.getCompoundTagAt(idx));
+            } else if ((list.getCompoundTagAt(idx)).hasKey(TAG_PLANT)) {
+                plant = ItemStack.loadItemStackFromNBT(list.getCompoundTagAt(idx));
+            }
+        }
     }
 
     @Override
@@ -32,18 +42,18 @@ public class TECarpentersFlowerPot extends TEBase {
 
         if (soil != null) {
             NBTTagCompound nbt1 = new NBTTagCompound();
-            nbt1.setByte("soil", TAG_SOIL);
+            nbt1.setByte(TAG_SOIL, (byte) 0);
             soil.writeToNBT(nbt1);
             list.appendTag(nbt1);
         }
         if (plant != null) {
             NBTTagCompound nbt1 = new NBTTagCompound();
-            nbt1.setByte("plant", TAG_PLANT);
+            nbt1.setByte(TAG_PLANT, (byte) 0);
             plant.writeToNBT(nbt1);
             list.appendTag(nbt1);
         }
 
-        nbt.setTag("pot_property", list);
+        nbt.setTag(TAG_PLANT_ITEMSTACKS, list);
     }
 
 }
