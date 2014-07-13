24f908147ceb5536de463782fc925ff5a620256c
Logan <kmartshopper@gmail.com>
2014-07-13 09:42:06 -0700

    Rewrite packet handling.

---
+++ src/main/java/carpentersblocks/network/ICarpentersPacket.java
@@ -0,0 +1,16 @@
+package carpentersblocks.network;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+
+import java.io.IOException;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+public interface ICarpentersPacket {
+
+    public void processData(EntityPlayer entityPlayer, ByteBufInputStream bbis) throws IOException;
+
+    public void appendData(ByteBuf buffer) throws IOException;
+
+}
