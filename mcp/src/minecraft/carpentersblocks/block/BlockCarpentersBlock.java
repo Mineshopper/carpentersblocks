89f66a25d44c13e1c2a7dfd2ce39e4b0dfd54608
Logan <kmartshopper@gmail.com>
2014-07-26 21:37:37 -0700

    Call super before doing anything else here.

--- mcp/src/minecraft/carpentersblocks/block/BlockCarpentersBlock.java
+++ mcp/src/minecraft/carpentersblocks/block/BlockCarpentersBlock.java
@@ -136,6 +136,8 @@ public class BlockCarpentersBlock extends BlockCoverable {
      */
     public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entityLiving, ItemStack itemStack)
     {
+        super.onBlockPlacedBy(world, x, y, z, entityLiving, itemStack);
+
         TEBase TE = getTileEntity(world, x, y, z);
 
         if (TE != null) {
@@ -160,8 +162,6 @@ public class BlockCarpentersBlock extends BlockCoverable {
 
             BlockProperties.setMetadata(TE, data);
         }
-
-        super.onBlockPlacedBy(world, x, y, z, entityLiving, itemStack);
     }
 
     @Override
