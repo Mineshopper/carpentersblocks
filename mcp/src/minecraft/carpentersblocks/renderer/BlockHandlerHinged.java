7c16d2036947cbb581042f06bc2e82856f7795a5
Logan <kmartshopper@gmail.com>
2014-08-14 14:42:58 -0700

    Make door pane render on opaque pass only.  Fixes #103.

--- mcp/src/minecraft/carpentersblocks/renderer/BlockHandlerHinged.java
+++ mcp/src/minecraft/carpentersblocks/renderer/BlockHandlerHinged.java
@@ -36,32 +36,34 @@ public class BlockHandlerHinged extends BlockHandlerBase {
      */
     protected final void renderPartPane(Icon icon, int x, int y, int z)
     {
-        int dir = side.ordinal();
-        float LIGHTNESS = LightingHelper.LIGHTNESS[dir];
+        if (renderPass == PASS_OPAQUE) {
+            int dir = side.ordinal();
+            float LIGHTNESS = LightingHelper.LIGHTNESS[dir];
 
-        Tessellator.instance.setBrightness(Block.glass.getMixedBrightnessForBlock(renderBlocks.blockAccess, x, y, z));
-        Tessellator.instance.setColorOpaque_F(LIGHTNESS, LIGHTNESS, LIGHTNESS);
+            Tessellator.instance.setBrightness(Block.glass.getMixedBrightnessForBlock(renderBlocks.blockAccess, x, y, z));
+            Tessellator.instance.setColorOpaque_F(LIGHTNESS, LIGHTNESS, LIGHTNESS);
 
-        renderBlocks.setRenderBounds(paneBounds[dir][0], paneBounds[dir][1], paneBounds[dir][2], paneBounds[dir][3], paneBounds[dir][4], paneBounds[dir][5]);
+            renderBlocks.setRenderBounds(paneBounds[dir][0], paneBounds[dir][1], paneBounds[dir][2], paneBounds[dir][3], paneBounds[dir][4], paneBounds[dir][5]);
 
-        switch (side) {
-            case DOWN:
-            case UP:
-                RenderHelper.renderFaceYNeg(renderBlocks, x, y, z, icon);
-                Tessellator.instance.setColorOpaque_F(LightingHelper.LIGHTNESS[1], LightingHelper.LIGHTNESS[1], LightingHelper.LIGHTNESS[1]);
-                RenderHelper.renderFaceYPos(renderBlocks, x, y, z, icon);
-                break;
-            case NORTH:
-            case SOUTH:
-                RenderHelper.renderFaceZNeg(renderBlocks, x, y, z, icon);
-                RenderHelper.renderFaceZPos(renderBlocks, x, y, z, icon);
-                break;
-            case WEST:
-            case EAST:
-                RenderHelper.renderFaceXNeg(renderBlocks, x, y, z, icon);
-                RenderHelper.renderFaceXPos(renderBlocks, x, y, z, icon);
-                break;
-            default: {}
+            switch (side) {
+                case DOWN:
+                case UP:
+                    RenderHelper.renderFaceYNeg(renderBlocks, x, y, z, icon);
+                    Tessellator.instance.setColorOpaque_F(LightingHelper.LIGHTNESS[1], LightingHelper.LIGHTNESS[1], LightingHelper.LIGHTNESS[1]);
+                    RenderHelper.renderFaceYPos(renderBlocks, x, y, z, icon);
+                    break;
+                case NORTH:
+                case SOUTH:
+                    RenderHelper.renderFaceZNeg(renderBlocks, x, y, z, icon);
+                    RenderHelper.renderFaceZPos(renderBlocks, x, y, z, icon);
+                    break;
+                case WEST:
+                case EAST:
+                    RenderHelper.renderFaceXNeg(renderBlocks, x, y, z, icon);
+                    RenderHelper.renderFaceXPos(renderBlocks, x, y, z, icon);
+                    break;
+                default: {}
+            }
         }
     }
 
