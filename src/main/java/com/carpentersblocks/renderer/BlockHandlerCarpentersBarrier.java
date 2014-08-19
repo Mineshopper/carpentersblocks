adb1b0584d58910bb16e0085dfc49da59b5845dc
Cruz Bishop <cruzjbishop@gmail.com>
2014-08-19 10:20:38 +1000

    Spacing fix
    I shouldn't be editing this inline, but I can't be bothered opening IDEA :c

--- src/main/java/com/carpentersblocks/renderer/BlockHandlerCarpentersBarrier.java
+++ src/main/java/com/carpentersblocks/renderer/BlockHandlerCarpentersBarrier.java
@@ -63,7 +63,7 @@ public class BlockHandlerCarpentersBarrier extends BlockHandlerBase {
     {
         BlockCarpentersBarrier tempBlock = (BlockCarpentersBarrier) srcBlock;
 
-        connect = new boolean[]  {
+        connect = new boolean[] {
                 tempBlock.canConnectBarrierTo(renderBlocks.blockAccess, x, y - 1, z, ForgeDirection.UP),
                 tempBlock.canConnectBarrierTo(renderBlocks.blockAccess, x, y + 1, z, ForgeDirection.DOWN),
                 tempBlock.canConnectBarrierTo(renderBlocks.blockAccess, x, y, z - 1, ForgeDirection.SOUTH),
@@ -76,7 +76,7 @@ public class BlockHandlerCarpentersBarrier extends BlockHandlerBase {
                 tempBlock.canConnectBarrierTo(renderBlocks.blockAccess, x + 1, y + 1, z, ForgeDirection.WEST),
         };
 
-        barrier = new boolean[]{
+        barrier = new boolean[] {
                 renderBlocks.blockAccess.getBlock(x, y - 1, z).equals(srcBlock),
                 renderBlocks.blockAccess.getBlock(x, y + 1, z).equals(srcBlock)
         };
