1cfcdc91817d0d00794f9e073e74a9a3d032a74a
Logan <kmartshopper@gmail.com>
2014-08-28 17:27:32 -0700

    Update version.

--- mcp/src/minecraft/com/carpentersblocks/CarpentersBlocks.java
+++ mcp/src/minecraft/com/carpentersblocks/CarpentersBlocks.java
@@ -28,7 +28,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 public class CarpentersBlocks {
 
     public static final String MODID = "CarpentersBlocks";
-    public static final String VERSION = "3.2.7 DEV R1";
+    public static final String VERSION = "3.2.7 DEV R2";
     public static CreativeTabs creativeTab = new CarpentersBlocksTab(MODID);
 
     @Instance(MODID)
