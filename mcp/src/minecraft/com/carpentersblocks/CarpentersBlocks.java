21eaa69d5323356903709632ef4426057fda34b5
Logan <kmartshopper@gmail.com>
2014-09-07 14:26:02 -0700

    Move dev to stable release.

--- mcp/src/minecraft/com/carpentersblocks/CarpentersBlocks.java
+++ mcp/src/minecraft/com/carpentersblocks/CarpentersBlocks.java
@@ -28,7 +28,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 public class CarpentersBlocks {
 
     public static final String MODID = "CarpentersBlocks";
-    public static final String VERSION = "3.2.7 DEV R4";
+    public static final String VERSION = "3.2.7";
     public static CreativeTabs creativeTab = new CarpentersBlocksTab(MODID);
 
     @Instance(MODID)
