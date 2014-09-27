84265f331714a61b4b1cc43c582ce8d2289b9fef
Mineshopper <kmartshopper@gmail.com>
2014-09-26 17:43:12 -0700

    Update version number.

--- src/main/java/com/carpentersblocks/CarpentersBlocks.java
+++ src/main/java/com/carpentersblocks/CarpentersBlocks.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 public class CarpentersBlocks {
 
     public static final String MODID = "CarpentersBlocks";
-    public static final String VERSION = "3.3.0 DEV R2";
+    public static final String VERSION = "3.3.0 DEV R3";
     public static FMLEventChannel channel;
     public static CreativeTabs creativeTab = new CarpentersBlocksTab(MODID);
 
