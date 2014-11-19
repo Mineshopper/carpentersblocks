11eda7a13de824a4d394f91f0059f9109f880d9d
Mineshopper <kmartshopper@gmail.com>
2014-11-18 23:02:12 -0800

    Pass slot with slope select packet. Hopefully fixes the last bug from this implementation.

--- src/main/java/com/carpentersblocks/network/PacketSlopeSelect.java
+++ src/main/java/com/carpentersblocks/network/PacketSlopeSelect.java
@@ -9,23 +9,26 @@ import com.carpentersblocks.block.BlockCarpentersSlope;
 
 public class PacketSlopeSelect implements ICarpentersPacket {
 
+    private int slot = 0;
     private boolean incDamage = false;
 
     public PacketSlopeSelect() {}
 
-    public PacketSlopeSelect(boolean incDamage)
+    public PacketSlopeSelect(int slot, boolean incDamage)
     {
+        this.slot = slot;
         this.incDamage = incDamage;
     }
 
     @Override
     public void processData(EntityPlayer entityPlayer, ByteBufInputStream bbis) throws IOException
     {
-        ItemStack itemStack = entityPlayer.getHeldItem();
+        int slot = bbis.readInt();
+        boolean incDmg = bbis.readBoolean();
+        ItemStack itemStack = entityPlayer.inventory.getStackInSlot(slot);
 
         if (itemStack != null) {
 
-            boolean incDmg = bbis.readBoolean();
             int maxDmg = BlockCarpentersSlope.slopeType.length - 1;
             int itemDmg = itemStack.getItemDamage();
             itemDmg += incDmg ? 1 : -1;
@@ -44,6 +47,7 @@ public class PacketSlopeSelect implements ICarpentersPacket {
     @Override
     public void appendData(ByteBuf buffer) throws IOException
     {
+        buffer.writeInt(slot);
         buffer.writeBoolean(incDamage);
     }
 
