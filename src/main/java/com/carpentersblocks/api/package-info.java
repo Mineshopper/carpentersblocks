8c9096006bf1fb6167b890e1f605fa41d4b5bd65
Shadowfacts <shadowfacts@users.noreply.github.com>
2015-08-03 13:45:20 -0400

    baka shadowfacts

--- src/main/java/com/carpentersblocks/api/package-info.java
+++ src/main/java/com/carpentersblocks/api/package-info.java
@@ -1,4 +1,4 @@
 @API(owner = "CarpentersBlocks", provides = "CarpentersBlocks|API", apiVersion = "3.3.7")
-package net.shadowfacts.yaw.api;
+package com.carpentersblocks.api;
 
 import cpw.mods.fml.common.API;
