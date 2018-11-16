52c505bb5b7ee64c78052326c25693865191724f
Logan <kmartshopper@gmail.com>
2018-11-15 19:23:11 -0800

    Update version to 3.4.0-poc.5 (suggested naming convention). Update to Forge version 1.12.2-14.23.5.2768.

--- src/main/java/com/carpentersblocks/renderer/ModelLoader.java
+++ src/main/java/com/carpentersblocks/renderer/ModelLoader.java
@@ -1,5 +1,7 @@
 package com.carpentersblocks.renderer;
 
+import java.util.function.Predicate;
+
 import com.carpentersblocks.CarpentersBlocks;
 import com.carpentersblocks.renderer.model.ModelBlock;
 import com.carpentersblocks.renderer.model.ModelCollapsibleBlock;
@@ -11,19 +13,20 @@ import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.client.resource.IResourceType;
 
 public class ModelLoader implements ICustomModelLoader {
 	
-    @Override
-    public boolean accepts(ResourceLocation resourceLocation) {
+	@Override
+	public boolean accepts(ResourceLocation resourceLocation) {
     	IModel model = null;
     	try { model = loadModel(resourceLocation); } catch (Exception ex) {}
     	return CarpentersBlocks.MOD_ID.equalsIgnoreCase(resourceLocation.getResourceDomain())
     			&& model != null;
-    }
+	}
 
-    @Override
-    public IModel loadModel(ResourceLocation resourceLocation) throws Exception {
+	@Override
+	public IModel loadModel(ResourceLocation resourceLocation) throws Exception {
     	if (resourceLocation.getResourcePath().equals(BlockRegistry.REGISTRY_NAME_BLOCK)) {
     		return new ModelBlock();
     	} else if (resourceLocation.getResourcePath().endsWith(BlockRegistry.REGISTRY_NAME_SLOPE)) {
@@ -35,9 +38,15 @@ public class ModelLoader implements ICustomModelLoader {
     	} else {
     		return null;
     	}
-    }
+	}
+	
+	/**
+	 * Won't build without this override
+	 */
+	@Override
+	public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) { }
 
-    @Override
-    public void onResourceManagerReload(IResourceManager resourceManager) { }
+	@Override
+	public void onResourceManagerReload(IResourceManager resourceManager) { }
     
 }
